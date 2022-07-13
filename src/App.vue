3e9355d5b85a9657e258d59aca7c1af3bd784cfc
TTpeach1 <TTpeach1@Outlook.com>
2022-07-13 12:07:38 +0800

    项目前期全部配置完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,90 @@
 <template>
   <div>
-    <h1>黑马头条</h1>
+    <van-nav-bar
+      title="购物车"
+      left-text="返回"
+      right-text="导航"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    />
+    <div class="ceshi">
+      <h3>测试rem</h3>
+    </div>
+    <van-card
+      num="2"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://img01.yzcdn.cn/vant/ipad.jpeg"
+    >
+      <template #tags>
+        <van-tag plain type="danger">标签</van-tag>
+        <van-tag plain type="danger">标签</van-tag>
+      </template>
+      <template #footer>
+        <van-button size="mini">按钮</van-button>
+        <van-button size="mini">按钮</van-button>
+      </template>
+    </van-card>
+    <van-card
+      num="2"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://img01.yzcdn.cn/vant/ipad.jpeg"
+    >
+      <template #tags>
+        <van-tag plain type="danger">标签</van-tag>
+        <van-tag plain type="danger">标签</van-tag>
+      </template>
+      <template #footer>
+        <van-button size="mini">按钮</van-button>
+        <van-button size="mini">按钮</van-button>
+      </template>
+    </van-card>
+    <van-card
+      num="2"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://img01.yzcdn.cn/vant/ipad.jpeg"
+    >
+      <template #tags>
+        <van-tag plain type="danger">标签</van-tag>
+        <van-tag plain type="danger">标签</van-tag>
+      </template>
+      <template #footer>
+        <van-button size="mini">按钮</van-button>
+        <van-button size="mini">按钮</van-button>
+      </template>
+    </van-card>
+    <!-- 底部 -->
+    <van-goods-action>
+      <van-goods-action-icon icon="chat-o" text="客服" />
+      <van-goods-action-icon icon="shop-o" text="店铺" />
+      <van-goods-action-button
+        color="#be99ff"
+        type="warning"
+        text="加入购物车"
+      />
+      <van-goods-action-button color="#7232dd" type="danger" text="立即购买" />
+    </van-goods-action>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HeimaModileApp'
-
 }
 </script>
 
 <style lang="less" scoped>
-
+.ceshi {
+  text-align: center;
+  width: 694px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入字体图标
+import '@/assets/font/iconfont.css'
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// rem适配
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
