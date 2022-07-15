b108a4f8454fe00399e89ed6f703523c892631c4
TTpeach1 <TTpeach1@Outlook.com>
2022-07-15 18:00:28 +0800

    底部导航栏路由完成

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,31 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    redirect: '/login'
-  },
-  {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/',
+    component: () => import('@/views/Layout'),
+    redirect: '/ ',
+    children: [
+      {
+        path: '/ ',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: '/video',
+        component: () => import('@/views/Video')
+      },
+      {
+        path: '/qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: '/profile',
+        component: () => import('@/views/My')
+      }
+    ]
   }
 ]
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item icon="home-o" to='/ '>
+        <template #icon>
+          <i class="iconfont icon-shouye"></i>
+          <span class="text">首页</span>
+        </template>
+      </van-tabbar-item>
+
+      <van-tabbar-item icon="home-o" to='/video'>
+        <template #icon>
+          <i class="iconfont icon-shipin"></i>
+          <span class="text">视频</span>
+        </template>
+      </van-tabbar-item>
+
+      <van-tabbar-item icon="home-o" to='/qa'>
+        <template #icon>
+          <i class="iconfont icon-wenda"></i>
+          <span class="text">问答</span>
+        </template>
+      </van-tabbar-item>
+
+      <van-tabbar-item icon="home-o" to='/profile'>
+        <template #icon>
+          <i class="iconfont icon-wode"></i>
+          <span class="text">我的</span>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+:deep(.van-tabbar-item__icon) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100%;
+}
+.iconfont {
+  font-size: .55rem;
+}
+.text {
+    font-size: 0.44rem;
+  }
+</style>
