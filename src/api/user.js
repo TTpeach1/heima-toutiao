5524aebe14403699394d0c0ff37a114abe71b269
TTpeach1 <TTpeach1@Outlook.com>
2022-07-16 16:44:42 +0800

    已完成我的页面

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,5 @@
 import request from '@/utils/request'
+// import store from '@/store'
 
 export const login = (mobile, code) => {
   return request({
@@ -12,5 +13,14 @@ export const timeBtnApi = (mobile) => {
   return request({
     url: `/v1_0/sms/codes/${mobile}`,
     method: 'GET'
+  })
+}
+
+export const getUserInfoApi = () => {
+  return request({
+    url: '/v1_0/user'
+    // headers: {
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
